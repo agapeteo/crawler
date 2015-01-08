4a0390d5c75b2c7409bc63ab16b314be4b0a2b6f
diverfd <diverfd@gmail.com>
2015-01-08 16:42:49 +0200

    v 0.1

--- src/main/java/NewBasicCrawler.java
+++ src/main/java/NewBasicCrawler.java
@@ -6,9 +6,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ForkJoinPool;
 import java.util.regex.Pattern;
 
 public class NewBasicCrawler {
@@ -22,68 +21,39 @@ public class NewBasicCrawler {
     private boolean internalOnly = true;
 
     public static void main(String[] args) {
-        NewBasicCrawler c = new NewBasicCrawler();
-        Set<String> initial = new HashSet();
+        ForkJoinPool pool = new ForkJoinPool();
+        List<String> initial = new ArrayList<String>();
         initial.add("https://code.google.com/p/lightcrawler/");
-//        initial.add("http://bbc.com");
-//        initial.add("http://habrahabr.ru");
-//        initial.add("http://en.wikipedia.org/wiki/Main_Page");
-        Set<String> retrievedLinks = c.retrieveLinksFromSite(0, initial);
-        for (String each : retrievedLinks) {
+        initial.add("http://bbc.com");
+        initial.add("http://habrahabr.ru");
+        initial.add("http://en.wikipedia.org/wiki/Main_Page");
+        UrlMultithreadParser parser = new UrlMultithreadParser(initial, true);
+//        do {
+//            System.out.printf("******************************************\n");
+//            System.out.printf("Main: Parallelism: %d\n", pool.getParallelism());
+//            System.out.printf("Main: Active Threads: %d\n", pool.getActiveThreadCount());
+//            System.out.printf("Main: Task Count: %d\n", pool.getQueuedTaskCount());
+//            System.out.printf("Main: Steal Count: %d\n", pool.getStealCount());
+//            System.out.printf("******************************************\n");
+//        } while ((!parser.isDone()));
+        Set<String> finalSet = new HashSet<String>(pool.invoke(parser));
+//
+        pool.shutdown();
+        for (String each : finalSet){
             System.out.println(each);
         }
-        System.out.println(retrievedLinks.size());
     }
 
-    private Set<String> retrieveLinksFromSite(int currentDepth, Set<String> initialLinks) {
-        if (currentDepth < depth) {
-            Set<String> localLinks = new HashSet<String>();
-            for (String link : initialLinks) {
-                Set<String> newLinks = parseURL(link.toLowerCase());
-                localLinks.addAll(retrieveLinksFromSite(currentDepth + 1, newLinks));
-            }
-            return localLinks;
-        } else {
-            return initialLinks;
-        }
-    }
-
-    public Set<String> parseURL(String url) {
-        Set<String> result = new HashSet<String>();
-        Connection connection = Jsoup.connect(url);
-        String host;
-        Document document;
-        try {
-            host = new URL(url).getHost();
-            document = connection.timeout(3000).get();
-        } catch (IOException e) {
-//            LOG.error("Can't connect to " + url, e); //todo: logging
-            return Collections.emptySet();
-        }
-        Elements links = document.select("a[href]");
-            for (Element link : links) {
-                String extractedLink = link.attr("abs:href");
-                if (internalOnly) {
-                    addInternalLink(extractedLink, host, result);
-                } else {
-                    addAnyLink(extractedLink, result);
-                }
-            }
-        return result;
-    }
-
-    private void addInternalLink(String extractedLink, String host, Set<String> result) {
-        String pattern = "^http(s)?://(www\\.)?" + host + "[\\w\\d\\W&&[^#]]+$";
-        if (Pattern.matches(pattern, extractedLink)
-                && !BINARY_TYPES.matcher(extractedLink.toLowerCase()).matches()) {
-            result.add(extractedLink);
-        }
-    }
-
-    private void addAnyLink(String extractedLink, Set<String> result) {
-        if (ANY_HOST_FILTER.matcher(extractedLink).matches()
-                && !BINARY_TYPES.matcher(extractedLink.toLowerCase()).matches()) {
-            result.add(extractedLink);
-        }
-    }
+//    private Set<String> retrieveLinksFromSite(int currentDepth, Set<String> initialLinks) {
+//        if (currentDepth < depth) {
+//            Set<String> localLinks = new HashSet<String>();
+//            for (String link : initialLinks) {
+//                Set<String> newLinks = parseURL(link.toLowerCase());
+//                localLinks.addAll(retrieveLinksFromSite(currentDepth + 1, newLinks));
+//            }
+//            return localLinks;
+//        } else {
+//            return initialLinks;
+//        }
+//    }
 }
---
+++ src/main/java/UrlMultithreadParser.java
@@ -0,0 +1,98 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.RecursiveTask;
+import java.util.regex.Pattern;
+
+public class UrlMultithreadParser extends RecursiveTask<List<String>> {
+    private static final int SEQUENTIAL_THRESHOLD = 1;
+    private static final Pattern ANY_HOST_FILTER = Pattern.compile("^http(s)?://(www\\.)?[\\w\\d\\W&&[^#]]+$");
+    private static final Pattern BINARY_TYPES = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g"
+            + "|png|tiff?|mid|mp2|mp3|mp4"
+            + "|wav|avi|mov|mpeg|ram|m4v|pdf"
+            + "|rm|svg|wmv|swf|wma|zip|rar|gz))$");
+    private final List<String> links;
+    private final boolean internalOnly;
+    private final int start;
+    private final int end;
+
+    public UrlMultithreadParser(List<String> links, boolean internalOnly) {
+        this(links, internalOnly, 0, links.size());
+    }
+
+    public UrlMultithreadParser(List<String> links, boolean internalOnly, int start, int end) {
+        this.links = links;
+        this.internalOnly = internalOnly;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected List<String> compute() {
+        int length = end - start;
+//        Set<String> retrievedLinks = new HashSet<String>();
+//        List<UrlMultithreadParser> tasks = new ArrayList<UrlMultithreadParser>();
+//        List<String> newLinks = Collections.EMPTY_LIST;
+        if (length == SEQUENTIAL_THRESHOLD){
+            return parseURL(links.get(start));
+        }
+        UrlMultithreadParser one = new UrlMultithreadParser(links, internalOnly, start, start + length/2);
+        one.fork();
+        UrlMultithreadParser two = new UrlMultithreadParser(links, internalOnly, start + length/2, end);
+        two.fork();
+
+        List<String> finalList = new ArrayList<String>();
+        List<String> newLinks1 = one.join();
+        List<String> newLinks2 = two.join();
+        finalList.addAll(newLinks1);
+        finalList.addAll(newLinks2);
+
+        return finalList;
+    }
+
+    private List<String> parseURL(String url) {
+        System.out.println("------->"+url);
+        List<String> result = new ArrayList<String>();
+        Connection connection = Jsoup.connect(url);
+        String host;
+        Document document;
+        try {
+            host = new URL(url).getHost();
+            document = connection.timeout(3000).get();
+        } catch (IOException e) {
+//            LOG.error("Can't connect to " + url, e); //todo: logging
+            return Collections.emptyList();
+        }
+        Elements links = document.select("a[href]");
+        for (Element link : links) {
+            String extractedLink = link.attr("abs:href");
+            if (internalOnly) {
+                addInternalLink(extractedLink, host, result);
+            } else {
+                addAnyLink(extractedLink, result);
+            }
+        }
+        return result;
+    }
+
+    private void addInternalLink(String extractedLink, String host, List<String> result) {
+        String pattern = "^http(s)?://(www\\.)?" + host + "[\\w\\d\\W&&[^#]]+$";
+        if (Pattern.matches(pattern, extractedLink)
+                && !BINARY_TYPES.matcher(extractedLink.toLowerCase()).matches()) {
+            result.add(extractedLink);
+        }
+    }
+
+    private void addAnyLink(String extractedLink, List<String> result) {
+        if (ANY_HOST_FILTER.matcher(extractedLink).matches()
+                && !BINARY_TYPES.matcher(extractedLink.toLowerCase()).matches()) {
+            result.add(extractedLink);
+        }
+    }
+}
